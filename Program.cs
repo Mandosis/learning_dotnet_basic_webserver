572be2ad679faa4a7b4cc26fde0aeeb8fa635da7
Chris Rabuse <rab233@live.com>
2016-11-20 17:07:39 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context => context.Response.WriteAsync("Hello MVA!"));
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+
+            host.Run();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# ASP.NET Core 1.1 Basic Web Server
+A basic ASP.NET Core web server using Kestrel
+
+
+## Setup
+
+### Requirements
+
+- .NET Core 1.1
+
+### Instructions
+
+Run the following commands:
+1. `git clone https://github.com/Mandosis/learn_dotnet_basic_server`
+2. `cd learn_dotenet_basic_server`
+3. `dotnet restore`
+4. `dotenet run`
+
+To watch files run `dotnet watch run`
